f5099bd9f9216bbea631d584c2a97de2d3219a74
youssef <shbanmhmdywsf8@gmail.com>
2022-11-11 17:23:37 +0200

    Task 5

---
+++ Task5/Task5.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+SERVICE_URL=http://localhost:80
+
+# To Get Only status number (200)
+RESPOSE=$(curl -o /dev/null -s -w "%{http_code}" $SERVICE_URL)
+
+#  http://localhost
+# curl --write-out '%{http_code}' --silent --output /dev/null
+
+if [ $RESPOSE = 200 ]
+then
+    echo Service Is Running.
+    exit 0
+else
+    echo Starting Service.....
+    docker-compose up -d
+    sleep 2
+    echo Done
+fi
