6bf2c4605afd97519f487318d6a3d3b76b13ae24
youssef <shbanmhmdywsf8@gmail.com>
2022-11-11 15:48:29 +0200

    Task3

---
+++ Task3/Task3.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+MYOS=$(hostnamectl | awk '/Operating/ { print $3 }' )
+
+# Install On Centos and RHEL
+if [ $MYOS = "Centos" || "RHEL" ]
+then
+    echo Setup Docker Using Yum
+    sudo yum update -y
+    sudo yum upgrade -y
+    sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+    sudo yum install curl docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
+
+    # Installing Docker Compose
+    sudo curl -SL https://github.com/docker/compose/releases/download/v2.12.2/docker-compose-linux-x86_64 -o /usr/local/bin/docker-compose
+    sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+
+    # Enable Service
+    sudo systemctl start docker
+	sudo systemctl enable --now docker
+fi
+echo Docker Up,Running On $MYOS.
+
+
+
+
+
+# Install On Ubuntu and debian
+
+if [ $MYOS = "Ubuntu" || "Debian" ]
+then
+    echo Setup Docker Using Apt
+    sudo apt update -y
+    sudo apt upgrade -y
+    sudo apt-get install docker curl  docker.io containerd runc -y
+
+    # Installing Docker Compose
+    sudo curl -SL https://github.com/docker/compose/releases/download/v2.12.2/docker-compose-linux-x86_64 -o /usr/local/bin/docker-compose
+    sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+
+
+    # Enable Service
+    sudo systemctl enable --now docker
+    sudo systemctl start docker
+fi
