79823f668f9c4c5d666fb775e4ee8e04c37ec275
Youssef Abu-Hamda <shbanmhmdywsf8@gmail.com>
2022-12-06 11:14:59 +0200

    task9

---
+++ Task9/pull-push_today.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+APP="youssef"
+REPO="https://github.com/yousabu/CICD-Kube-Docker.git"
+BRANCH="main"
+
+git clone -b $BRANCH --depth=1 $REPO $APP
+cd  ./$APP
+
+git checkout main
+docker build . -t $APP:$(date +"%d-%m-%y")
+
+docker login registry.gitlab.com -u <USER> -p <PASSWORD>
+docker push $APP:$(date +"%d-%m-%y")
+
+echo "Done"
